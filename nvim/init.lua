76a091802367d67ec92578f02446d83c3014a34e
Joao Gomes <joaopfg.2002@gmail.com>
2026-02-23 00:03:25 +0000

    add session persistance

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,16 @@ vim.g.mapleader = " " -- Leader = Space
 vim.opt.termguicolors = true -- Needed for modern themes
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
 
+-- Set indentation to 4 spaces
+vim.opt.tabstop = 4       -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 4    -- Number of spaces used for autoindent
+vim.opt.softtabstop = 4   -- Number of spaces per Tab when editing
+vim.opt.expandtab = true  -- Use spaces instead of actual tab characters
+
+-- Optional: enable autoindent
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
 -- ===========================
 -- lazy.nvim setup
 -- ===========================
@@ -12,70 +22,64 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ 
--- ===========================
--- Theme: naysayer-colors
--- ===========================
-{
-    "whizikxd/naysayer-colors.nvim",
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme("naysayer")
-    end
-}, 
--- ===========================
--- File Explorer: nvim-tree
--- ===========================
-{
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-        require("nvim-tree").setup {
-            view = {
-                width = 30,
-                side = "left", -- sidebar on left
-                preserve_window_proportions = true
-            },
-            renderer = {
-                icons = {
-                    show = {
-                        file = false,
-                        folder = false,
-                        git = false
-                    }
-                }
-            },
-            update_focused_file = {
-                enable = true,
-                update_cwd = false
-            },
-            hijack_cursor = false,
-            hijack_directories = {
-                enable = true,
-                auto_open = true
-            },
-            actions = {
-                open_file = {
-                    quit_on_open = false, -- keep explorer open
-                    window_picker = {
-                        enable = false
-                    } -- avoid picking explorer
-                }
+require("lazy").setup({
+    -- ===========================
+    -- Theme: naysayer-colors
+    -- ===========================
+    {
+        "whizikxd/naysayer-colors.nvim",
+        lazy = false,
+        config = function()
+            vim.cmd.colorscheme("naysayer")
+        end
+    },
+    -- ===========================
+    -- File Explorer: nvim-tree
+    -- ===========================
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = function()
+            require("nvim-tree").setup {
+                view = { width = 30, side = "left", preserve_window_proportions = true },
+                renderer = { icons = { show = { file = false, folder = false, git = false } } },
+                update_focused_file = { enable = true, update_cwd = false },
+                hijack_cursor = false,
+                hijack_directories = { enable = true, auto_open = true },
+                actions = { open_file = { quit_on_open = false, window_picker = { enable = false } } }
             }
-        }
-    end
-}, 
--- ===========================
--- LazyGit integration
--- ===========================
-{
-    "kdheepak/lazygit.nvim",
-    lazy = true, -- load on demand
-    cmd = "LazyGit",
-    config = function()
-        vim.g.lazygit_floating_window_winblend = 0
-        vim.g.lazygit_floating_window_scaling_factor = 0.9
-    end
-}})
+        end
+    },
+    -- ===========================
+    -- LazyGit integration
+    -- ===========================
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = "LazyGit",
+        config = function()
+            vim.g.lazygit_floating_window_winblend = 0
+            vim.g.lazygit_floating_window_scaling_factor = 0.9
+        end
+    },
+    -- ===========================
+    -- Persistence (auto session)
+    -- ===========================
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = {}, -- default options
+        config = function()
+            -- Automatically load last session if no file argument
+            vim.api.nvim_create_autocmd("VimEnter", {
+                callback = function()
+                    if vim.fn.argc() == 0 then
+                        require("persistence").load()
+                    end
+                end
+            })
+        end
+    }
+})
 
 -- ===========================
 -- Keymaps
@@ -138,6 +142,40 @@ vim.keymap.set("n", "j", "gj", {
 -- k → gk (move up by visual line when wrapped)
 vim.keymap.set("n", "k", "gk", {
     desc = "Move up by visual line"
+})
+
+-- Toggle comment for selected lines (C/C++ style)
+vim.keymap.set("v", "<leader>/", function()
+    local start_line = vim.fn.line("'<")
+    local end_line = vim.fn.line("'>")
+
+    -- Get the lines in the selection
+    local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+
+    -- Determine if all lines are already commented
+    local all_commented = true
+    for _, line in ipairs(lines) do
+        if not line:match("^%s*//") then
+            all_commented = false
+            break
+        end
+    end
+
+    -- Toggle comment
+    for i, line in ipairs(lines) do
+        if all_commented then
+            -- Uncomment
+            lines[i] = line:gsub("^(%s*)//", "%1")
+        else
+            -- Comment
+            lines[i] = "//" .. line
+        end
+    end
+
+    -- Set the modified lines back
+    vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, lines)
+end, {
+    desc = "Toggle comment for selection"
 })
 
 -- ===========================
